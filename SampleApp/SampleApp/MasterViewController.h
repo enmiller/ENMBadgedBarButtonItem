d71c173409666f822c3698b273b77d8a415e9f01
Eric Miller <eric.miller@mutualmobile.com>
2014-06-03 10:55:12 -0500

    Added the new SampleApp project

---
+++ SampleApp/SampleApp/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  TestBadge
+//
+//  Created by Eric Miller on 5/21/14.
+//  Copyright (c) 2014 Frozen Panda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
